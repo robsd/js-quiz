c4697e3928d4734990c7a589e53c3aee226475cc
Robert Dixon <79385125+robsd@users.noreply.github.com>
2025-04-09 18:01:25 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const checkAnswers = () => {
+    const ansKey = {
+        1: 'd',
+        2: 'b',
+        3: 'a'
+    };
+
+    const qs = Object.keys(ansKey);
+    let correct = 0;
+    const feedback = document.querySelector('#feedback');
+
+    for (const q of qs) {
+        const ans = document.querySelector(`input[name="q${q}"]:checked`);
+        
+        if (!ans) {
+            feedback.textContent = 'You have unanswered questions!';
+            feedback.className = 'text-danger';
+            return;
+        }
+
+        if (ans.value == ansKey[q]) {
+            correct++;
+        }
+    }
+
+    if (correct === qs.length) {
+        feedback.textContent = 'All questions are correct!';
+        feedback.className = 'text-success';
+    }
+    else {
+        feedback.textContent = `You got ${correct} out of ${qs.length} questions correct.`;
+        feedback.className = 'text-danger';
+    }
+};
